6066ee31407ffe62ee67869d636f88ee1d9f94c5
hesy87 <hesam.mahmoodi@gmail.com>
2023-07-25 01:07:27 +0330

    update  buttom

--- src/components/UI/Button/index.js
+++ src/components/UI/Button/index.js
@@ -1,58 +1,19 @@
 import React, { useState } from "react";
-import { PoweroffOutlined } from "@ant-design/icons";
-import { Button, Space } from "antd";
-const App = () => {
-  const [loadings, setLoadings] = useState([]);
-  const enterLoading = (index) => {
-    setLoadings((prevLoadings) => {
-      const newLoadings = [...prevLoadings];
-      newLoadings[index] = true;
-      return newLoadings;
-    });
-    setTimeout(() => {
-      setLoadings((prevLoadings) => {
-        const newLoadings = [...prevLoadings];
-        newLoadings[index] = false;
-        return newLoadings;
-      });
-    }, 6000);
-  };
-  return (
-    <Space direction="vertical">
-      <Space wrap>
-        <Button type="primary" loading>
-          Loading
-        </Button>
-        <Button type="primary" size="small" loading>
-          Loading
-        </Button>
-        <Button type="primary" icon={<PoweroffOutlined />} loading />
-      </Space>
+import { PoweroffOutlined, FieldTimeOutlined } from "@ant-design/icons";
+import { Button } from "antd";
 
-      <Space wrap>
-        <Button
-          type="primary"
-          loading={loadings[0]}
-          onClick={() => enterLoading(0)}
-        >
-          Click me!
-        </Button>
-        <Button
-          type="primary"
-          icon={<PoweroffOutlined />}
-          loading={loadings[1]}
-          onClick={() => enterLoading(1)}
-        >
-          Click me!
-        </Button>
-        <Button
-          type="primary"
-          icon={<PoweroffOutlined />}
-          loading={loadings[2]}
-          onClick={() => enterLoading(2)}
-        />
-      </Space>
-    </Space>
+const App = () => {
+  const enterLoading = () => {};
+  return (
+    <Button
+      type="primary"
+      icon={<FieldTimeOutlined />}
+      onClick={() => enterLoading()}
+    >
+      START !
+    </Button>
   );
 };
 export default App;
+
+/* <PoweroffOutlined />; */
